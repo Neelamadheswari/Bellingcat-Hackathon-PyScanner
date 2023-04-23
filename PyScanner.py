005a0a091be5fd0a47a1907b4d7a58bde107ddb0
Neela Madheswari <neela.madheswari@gmail.com>
2023-04-23 21:21:59 +0530

    Update PyScanner.py

--- PyScanner.py
+++ PyScanner.py
@@ -7,16 +7,15 @@ n = int(input('Enter the maximum number of ports to be scanned: '))
 t1 = socket.gethostbyname(target)
 def py_scan(port):
     try:
-        s.connect((target_ip, port))
+        s.connect((t1, port))
         return True
     except:
         return False
 start = time.time()
-# scanning port 0 to n-1
-for port in range(n):
-    if py_scan(port):
-        print(f'port {port} is open')
+for i in range(n):
+    if py_scan(i):
+        print(f'port {i} is open')
     else:
-        print(f'port {port} is closed')
+        print(f'port {i} is closed')
 end = time.time()
 print(f'Time taken {end-start:.2f} seconds')
